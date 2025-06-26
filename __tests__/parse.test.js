17be2e56bb33f42f11e0fe16f8b51a12db17d1cc
she1nXgod <domenator156@mail.ru>
2025-06-26 16:16:56 +0300

    add tests for yaml files

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -4,3 +4,9 @@ import genDiff from '../index.js';
 test('json', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
 });
+test('yaml', () => {
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(result);
+});
+test('yml', () => {
+  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(result);
+});
