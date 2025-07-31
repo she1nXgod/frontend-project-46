95a8bd9774a2be22ed75a5fb1758af083278a6f2
she1nXgod <domenator156@mail.ru>
2025-07-31 14:10:42 +0300

    add tests for plain format

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -1,12 +1,27 @@
-import result from '../__fixtures__/result.js';
+import resultStylish from '../__fixtures__/resultStylish.js';
+import resultPlain from '../__fixtures__/resultPlain.js';
 import genDiff from '../index.js';
 
-test('json', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
+describe('stylish format', () => {
+  test('json', () => {
+    expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(resultStylish);
+  });
+  test('yaml', () => {
+    expect(genDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml')).toBe(resultStylish);
+  });
+  test('yml', () => {
+    expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toBe(resultStylish);
+  });
 });
-test('yaml', () => {
-  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(result);
-});
-test('yml', () => {
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(result);
+
+describe('plain format', () => {
+  test('json', () => {
+    expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain')).toBe(resultPlain);
+  });
+  test('yaml', () => {
+    expect(genDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml', 'plain')).toBe(resultPlain);
+  });
+  test('yml', () => {
+    expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml', 'plain')).toBe(resultPlain);
+  });
 });
