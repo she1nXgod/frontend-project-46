3f23e02c3c6c21daecf243e4279e670c74bf7fd8
she1nXgod <domenator156@mail.ru>
2025-08-02 13:49:58 +0300

    fix

--- src/parsers.js
+++ src/parsers.js
@@ -5,12 +5,16 @@ import yaml from 'js-yaml';
 const fileParser = (filepath) => {
   const readFile = fs.readFileSync(path.resolve(process.cwd(), filepath));
   const format = path.extname(filepath);
-  if (format === '.json') {
-    return JSON.parse(readFile);
-  } else if (format === '.yaml') {
-    return yaml.load(readFile);
-  } else if (format === '.yml') {
-    return yaml.load(readFile);
+
+  switch (format) {
+    case '.json':
+      return JSON.parse(readFile);
+    case '.yaml':
+      return yaml.load(readFile);
+    case '.yml':
+      return yaml.load(readFile);
+    default:
+      return 'Unsupported format';
   }
 };
 
